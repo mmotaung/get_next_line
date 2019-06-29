d0d3d54121c3259eba44c139d7372c8d68c0f5a8
Modikadika MOTAUNG <mmotaung@c4r10s1.wethinkcode.co.za>
2019-06-29 11:01:05 +0200

    working

---
+++ main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmotaung <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/29 07:07:16 by mmotaung          #+#    #+#             */
+/*   Updated: 2019/06/29 07:12:13 by mmotaung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	(void)argc;
+	fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line) == 1)
+	{
+		printf("%s\n", line);
+		if (line)
+			ft_strdel(&line);
+	}
+	if (line)
+		free(line);
+	close(fd);
+	return (0);
+}
